a5c045e6ffe2f9144f0866a62567ea3865dec540
Harsh Solanki <harsh.solanki2105@gmail.com>
2023-09-29 17:45:35 +0530

    dfs added

--- Main.java
+++ Main.java
@@ -19,19 +19,19 @@ public class Main {
 
     
 
-    public static void main(String[] args) {
-        root = new Node(3);
-        root.left = new Node(4);
-        root.right = new Node(8);
-        root.left.left = new Node(12);
-        root.left.right = new Node(19);
-        root.right.left = new Node(7);
-        root.right.right = new Node(11);
-        root.left.right.left = new Node(36);
-        root.left.right.right = new Node(9);
-        root.right.right.left = new Node(2);
-        root.right.right.left.right = new Node(44);
-
-    }
+//    public static void main(String[] args) {
+//        root = new Node(3);
+//        root.left = new Node(4);
+//        root.right = new Node(8);
+//        root.left.left = new Node(12);
+//        root.left.right = new Node(19);
+//        root.right.left = new Node(7);
+//        root.right.right = new Node(11);
+//        root.left.right.left = new Node(36);
+//        root.left.right.right = new Node(9);
+//        root.right.right.left = new Node(2);
+//        root.right.right.left.right = new Node(44);
+//
+//    }
 
 }
---
+++ Tree/BinaryTree/MAANG_Ques/DFS.java
@@ -0,0 +1,41 @@
+package Tree.BinaryTree.MAANG_Ques;
+
+public class DFS {
+
+    static Node root;
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Diameter of Binary Tree
+    static int diameter(Node root) {
+
+    }
+
+    public static void main(String[] args) {
+        root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.left.left.left = new Node(7);
+        root.left.left.right = new Node(8);
+        root.left.right.right = new Node(9);
+        root.left.left.right.right = new Node(10);
+        root.left.right.right.left = new Node(11);
+        root.left.left.right.right.left = new Node(12);
+        root.left.right.right.left.right = new Node(13);
+        root.left.left.right.right.left.left = new Node(14);
+    }
+
+}
