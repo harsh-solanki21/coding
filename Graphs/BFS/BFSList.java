b4a488c21b2015ee16fa819c6a3d2a8d5fc59267
Harsh Solanki <harsh.solanki2105@gmail.com>
2023-10-27 17:42:40 +0530

    BFS updated

--- Graphs/BFS/BFSList.java
+++ Graphs/BFS/BFSList.java
@@ -2,6 +2,7 @@ package Graphs.BFS;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class BFSList {
@@ -33,7 +34,7 @@ public class BFSList {
         queue.add(new Pair(source, source + ""));
         boolean[] visited = new boolean[vertices];
 
-        while (queue.size() > 0) {
+        while (!queue.isEmpty()) {
             // Remove - Mark Visited - Work(Print) - Add Unmarked neighbours
             Pair rem = queue.removeFirst();
 
@@ -56,14 +57,14 @@ public class BFSList {
     static ArrayList<Integer> bfsTraversal(ArrayList<ArrayList<Integer>> graph, int vertices) {
         ArrayList<Integer> ans = new ArrayList<>();
         boolean[] visited = new boolean[vertices];
-        Queue<Integer> q = new ArrayDeque<>();
+        Queue<Integer> q = new LinkedList<>();
         q.add(0);
         visited[0] = true;
 
         while (!q.isEmpty()) {
-            int node = q.poll();
-            ans.add(node);
-            for (int i : graph.get(node)) {
+            int vertex = q.poll();
+            ans.add(vertex);
+            for (int i : graph.get(vertex)) {
                 if (!visited[i]) {
                     q.add(i);
                     visited[i] = true;
@@ -113,17 +114,33 @@ public class BFSList {
         // Time Complexity: O(V) + O(2E) = O(V + E)
         // Space Complexity: O(3V) = O(V)
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < vertices; i++) {
             adj.add(new ArrayList<>());
         }
+
+        /*
+                1 ---- 3
+              /        |  \
+             0         |    5 --- 6
+              \        |  /
+                2 ---- 4
+        */
         adj.get(0).add(1);
+        adj.get(0).add(2);
         adj.get(1).add(0);
-        adj.get(0).add(4);
-        adj.get(4).add(0);
-        adj.get(1).add(2);
-        adj.get(2).add(1);
         adj.get(1).add(3);
+        adj.get(2).add(0);
+        adj.get(2).add(4);
         adj.get(3).add(1);
+        adj.get(3).add(4);
+        adj.get(3).add(5);
+        adj.get(4).add(2);
+        adj.get(4).add(3);
+        adj.get(4).add(5);
+        adj.get(5).add(3);
+        adj.get(5).add(4);
+        adj.get(5).add(6);
+        adj.get(6).add(5);
 
         System.out.println(bfsTraversal(adj, vertices));
 
---
+++ Graphs/BFS/BFSListDisconnected.java
@@ -0,0 +1,81 @@
+package Graphs.BFS;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFSListDisconnected {
+
+    static class Edge {
+        int source, destination;
+
+        Edge(int source, int destination) {
+            this.source = source;
+            this.destination = destination;
+        }
+    }
+
+
+    // TC - O(V + E)
+    static void bfs(ArrayList<Edge>[] graph, boolean[] visited, int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            int vertex = queue.remove();
+            if (!visited[vertex]) {
+                System.out.print(vertex + " ");
+                visited[vertex] = true;
+
+                for (int i = 0; i < graph[vertex].size(); i++) {
+                    Edge e = graph[vertex].get(i);
+                    queue.add(e.destination);
+                }
+            }
+        }
+    }
+
+
+    static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 3));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 2));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 1));
+        graph[2].add(new Edge(2, 3));
+
+        graph[3].add(new Edge(3, 0));
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 2));
+
+        graph[4].add(new Edge(4, 5));
+
+        graph[5].add(new Edge(5, 4));
+    }
+
+
+    public static void main(String[] args) {
+        int vertices = 7;
+        ArrayList<Edge>[] graph = new ArrayList[vertices];
+        createGraph(graph);
+        /*
+              0 --- 1
+              |  /  |     4 --- 5    6
+              3 --- 2
+         */
+        boolean[] visited = new boolean[vertices];
+        for (int i = 0; i < vertices; i++) {
+            if (!visited[i]) {
+                bfs(graph, visited, i);
+            }
+        }
+    }
+    
+}
