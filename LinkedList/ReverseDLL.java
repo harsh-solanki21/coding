cb069b26a46e0da0599278dea8ebcc4393876b86
Harsh Solanki <harsh.solanki2105@gmail.com>
2023-11-20 15:19:42 +0530

    ReverseDLL added

---
+++ LinkedList/ReverseDLL.java
@@ -0,0 +1,58 @@
+package LinkedList;
+
+public class ReverseDLL {
+
+    static Node head;
+
+    static class Node {
+        int data;
+        Node next, prev;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    static Node reverseDLL(Node head) {
+        Node temp = null;
+        Node current = head;
+
+        while (current != null) {
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+            current = current.prev;
+        }
+
+        if (temp != null) {
+            head = temp.prev;
+        }
+
+        return head;
+    }
+
+    static void display(Node head) {
+        Node temp = head;
+        System.out.print("null <-> ");
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.prev = head;
+        head.next.next = new Node(3);
+        head.next.next.prev = head.next.next;
+
+        display(head);
+        Node newHead = reverseDLL(head);
+//        display(newHead);
+    }
+
+}
