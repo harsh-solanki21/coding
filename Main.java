269d3ab3f6934b9182f161e002a7e82ffb301a97
Harsh Solanki <harsh.solanki@reynasolutions.com>
2024-01-10 18:10:20 +0530

    Main Updated

--- Main.java
+++ Main.java
@@ -2,50 +2,50 @@ import java.util.*;
 
 public class Main {
 
-    static class Pair {
-        int i, j;
+    static class Node {
+        int x, y;
 
-        Pair(int i, int j) {
-            this.i = i;
-            this.j = j;
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 
     static int orangesRotting(int[][] grid) {
-        Queue<Pair> q = new LinkedList<>();
-        int minutes = 0;
+        Queue<Node> q = new LinkedList<>();
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == 2) {
-                    q.add(new Pair(i, j));
+                    q.add(new Node(i, j));
                 }
             }
         }
 
-        int[] x = {-1, 0, 1, 0};
-        int[] y = {0, 1, 0, -1};
+        int[] a = {-1, 0, 1, 0};
+        int[] b = {0, 1, 0, -1};
+        int time = -1;
         while (!q.isEmpty()) {
-            int currentSize = q.size();
-            for (int i = 0; i < currentSize; i++) {
-                Pair remQ = q.remove();
-                minutes++;
-                for(int j = 0; j<4; j++) {
-                    int first = remQ.i + x[i];
-                    int second = remQ.j + y[i];
-                    if(first >= 0 && second >= 0 && first < grid.length && second < grid[0].length && grid[first][second] == 1) {
-                        grid[first][second] = 2;
-                        q.add(new Pair());
+            int currSize = q.size();
+            time++;
+            for (int i = 0; i < currSize; i++) {
+                Node rem = q.remove();
+                for (int j = 0; j < 4; j++) {
+                    int row = rem.x + a[j];
+                    int col = rem.y + b[j];
+                    if (row >= 0 && col >= 0 && row < grid.length && col < grid[0].length && grid[row][col] == 1) {
+                        grid[row][col] = 2;
+                        q.add(new Node(row, col));
                     }
                 }
             }
         }
 
-        return minutes;
+        return time;
     }
 
 
     public static void main(String[] args) {
-        int[][] grid = {{2, 1, 1}, {1, 1, 0}, {0, 1, 1}};
+        int[][] grid = {{2, 1, 1}, {0, 1, 1}, {1, 0, 1}};
         System.out.println(orangesRotting(grid));
     }
 
