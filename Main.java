f57a5cfd12a2b982a34fec7fbe6567cfb9e0437e
Harsh Solanki <harsh.solanki@reynasolutions.com>
2024-02-01 16:57:24 +0530

    Main Updated

--- Main.java
+++ Main.java
@@ -2,116 +2,110 @@ import java.util.*;
 
 public class Main {
 
-    static void nextPermutation(int[] nums) {
-        int index1 = -1;
-        int index2 = -1;
+    static class Node {
+        int x, y, distance;
 
-        // step 1 find breaking point
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                index1 = i;
-                break;
+        Node(int x, int y, int distance) {
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
+        }
+    }
+
+    static int[][] updateMatrix(int[][] mat) {
+        Queue<Node> q = new LinkedList<>();
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] != 0) {
+                    q.offer(new Node(i, j, 0));
+                }
             }
         }
 
-        // if there is no breaking  point
-        if (index1 == -1) {
-            reverse(nums, 0);
-            return;
-        }
-
-        // step 2 find next greater element and swap with index2
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (nums[i] > nums[index1]) {
-                index2 = i;
-                break;
+        int[] row = {-1, 0, 1, 0};
+        int[] col = {0, 1, 0, -1};
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int first = node.x + row[i];
+                int second = node.y + col[i];
+                if (first >= 0 && second >= 0 && first < mat.length && second < mat[0].length) {
+                    if (mat[first][second] == 0) {
+                        mat[node.x][node.y] = node.distance + 1;
+                        break;
+                    } else {
+                        q.add(new Node(first, second, node.distance + 1));
+                    }
+                }
             }
         }
 
-        swap(nums, index1, index2);
-        // step 3 reverse the rest right half
-        reverse(nums, index1 + 1);
-
-    }
-
-    private static void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    private static void reverse(int[] nums, int start) {
-        int i = start;
-        int j = nums.length - 1;
-        while (i < j) {
-            swap(nums, i, j);
-            i++;
-            j--;
-        }
+        return mat;
     }
 
 
     public static void main(String[] args) {
-        int[] prices = {1, 1, 5};
-        nextPermutation(prices);
-        System.out.println(Arrays.toString(prices));
+        int[][] mat = {{0, 0, 0}, {0, 1, 0}, {1, 1, 1}};
+        int[][] ans = updateMatrix(mat);
+        for (int[] i : ans) {
+            System.out.println(Arrays.toString(i));
+        }
     }
 
-//    static class Node {
-//        int parent, node;
+//    static void nextPermutation(int[] nums) {
+//        int index1 = -1;
+//        int index2 = -1;
 //
-//        Node(int parent, int node) {
-//            this.parent = parent;
-//            this.node = node;
+//        // step 1 find breaking point
+//        for (int i = nums.length - 2; i >= 0; i--) {
+//            if (nums[i] < nums[i + 1]) {
+//                index1 = i;
+//                break;
+//            }
+//        }
+//
+//        // if there is no breaking  point
+//        if (index1 == -1) {
+//            reverse(nums, 0);
+//            return;
+//        }
+//
+//        // step 2 find next greater element and swap with index2
+//        for (int i = nums.length - 1; i >= 0; i--) {
+//            if (nums[i] > nums[index1]) {
+//                index2 = i;
+//                break;
+//            }
+//        }
+//
+//        swap(nums, index1, index2);
+//        // step 3 reverse the rest right half
+//        reverse(nums, index1 + 1);
+//
+//    }
+//
+//    private static void swap(int[] nums, int i, int j) {
+//        int temp = nums[i];
+//        nums[i] = nums[j];
+//        nums[j] = temp;
+//    }
+//
+//    private static void reverse(int[] nums, int start) {
+//        int i = start;
+//        int j = nums.length - 1;
+//        while (i < j) {
+//            swap(nums, i, j);
+//            i++;
+//            j--;
 //        }
 //    }
-
-//    static boolean cycleDetectionBFS(ArrayList<ArrayList<Integer>> graph, int vertices, int source) {
 //
-//    }
-
-
-//    static boolean cycleDetectionDFS(ArrayList<ArrayList<Integer>> graph, int vertices, int source) {
 //
-//    }
-
-
 //    public static void main(String[] args) {
-//
-////        0 -- 1 --- 4
-////        |    |    |  \
-////        2    3    5 - 6
-//
-//        int vertices = 7;
-//        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-//
-//        for (int i = 0; i < vertices; i++) {
-//            graph.add(new ArrayList<>());
-//        }
-//
-//        graph.get(0).add(1);
-//        graph.get(0).add(2);
-//
-//        graph.get(1).add(0);
-//        graph.get(1).add(3);
-//        graph.get(1).add(4);
-//
-//        graph.get(2).add(0);
-//
-//        graph.get(3).add(1);
-//
-//        graph.get(4).add(1);
-//        graph.get(4).add(5);
-//        graph.get(4).add(6);
-//
-//        graph.get(5).add(4);
-//        graph.get(5).add(6);
-//
-//        graph.get(6).add(4);
-//        graph.get(6).add(5);
-//
-////        System.out.println(cycleDetectionBFS(graph, vertices, 0));
-//        System.out.println(cycleDetectionDFS(graph, vertices, 0));
+//        int[] prices = {1, 1, 5};
+//        nextPermutation(prices);
+//        System.out.println(Arrays.toString(prices));
 //    }
 
 
