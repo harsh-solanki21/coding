97f0b6b3e6d56f03e3b195a3c21b142a8b0a3c95
Harsh Solanki <harsh.solanki@reynasolutions.com>
2024-02-02 11:42:28 +0530

    Matrix01 Updated

--- Graphs/Matrix01.java
+++ Graphs/Matrix01.java
@@ -1,45 +1,48 @@
 package Graphs;
 
-import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class Matrix01 {
 
     // https://leetcode.com/problems/01-matrix/
-    static class Pair {
-        int first, second, steps;
+    // can be solved using DP
+    static class Node {
+        int x, y, distance;
 
-        Pair(int first, int second, int stpes) {
-            this.first = first;
-            this.second = second;
-            this.steps = stpes;
+        Node(int x, int y, int distance) {
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
         }
     }
 
     static int[][] updateMatrix(int[][] mat) {
-        Queue<Pair> q = new ArrayDeque<>();
+        boolean[][] visited = new boolean[mat.length][mat[0].length];
+        Queue<Node> q = new LinkedList<>();
+
         for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[0].length; j++) {
-                if (mat[i][j] != 0)
-                    q.add(new Pair(i, j, 0));
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] == 0) {
+                    q.offer(new Node(i, j, 0));
+                    visited[i][j] = true;
+                }
             }
         }
 
         int[] row = {-1, 0, 1, 0};
         int[] col = {0, 1, 0, -1};
         while (!q.isEmpty()) {
-            int first = q.peek().first;
-            int second = q.peek().second;
-            int steps = q.peek().steps;
-            q.remove();
-
+            Node node = q.poll();
             for (int i = 0; i < 4; i++) {
-                int x = first + row[i];
-                int y = second + col[i];
-                if (x >= 0 && y >= 0 && x < mat.length && y < mat[0].length) {
-                    if (mat[x][y] == 0) {
-                        mat[x][y] = steps;
+                int first = node.x + row[i];
+                int second = node.y + col[i];
+                if (first >= 0 && second >= 0 && first < mat.length && second < mat[0].length) {
+                    if (!visited[first][second]) {
+                        mat[first][second] = node.distance + 1;
+                        visited[first][second] = true;
+                        q.offer(new Node(first, second, node.distance + 1));
                     }
                 }
             }
@@ -50,7 +53,7 @@ public class Matrix01 {
 
 
     public static void main(String[] args) {
-        int[][] mat = {{0, 0, 0}, {0, 1, 0}, {1, 1, 1}};
+        int[][] mat = {{0, 0, 1}, {1, 1, 0}, {1, 1, 1}};
         int[][] ans = updateMatrix(mat);
         for (int[] i : ans) {
             System.out.println(Arrays.toString(i));
