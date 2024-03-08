cc3be24329cef4e096f70d7e69fcd046aae669f6
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-03-08 17:33:10 +0530

    BFS technique added in EventualSafeState

--- Graphs/DFS/EventualSafeState.java
+++ Graphs/DFS/EventualSafeState.java
@@ -1,7 +1,9 @@
 package Graphs.DFS;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class EventualSafeState {
 
@@ -92,6 +94,55 @@ public class EventualSafeState {
     }
 
 
+
+    // BFS - Kahn's Algorithm (Topological Sort)
+    static List<Integer> eventualSafeNodesBFS(int[][] graph) {
+        int n = graph.length;
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        int[] indegree = new int[graph.length];
+        for (int i = 0; i < n; i++) {
+            for (int j : graph[i]) {
+                adj.get(j).add(i);
+                indegree[i]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        boolean[] safe = new boolean[n];
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            safe[node] = true;
+
+            for (int neighbor : adj.get(node)) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.add(neighbor);
+                }
+            }
+        }
+
+        List<Integer> safeNodes = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (safe[i]) {
+                safeNodes.add(i);
+            }
+        }
+
+        return safeNodes;
+    }
+
+
+
     public static void main(String[] args) {
         // 1. ArrayList<ArrayList<Integer>>
 
@@ -139,6 +190,8 @@ public class EventualSafeState {
 //        int[][] graph2 = {{1, 2}, {2, 3}, {5}, {0}, {5}, {}, {}};
         int[][] graph2 = {{1}, {2}, {3}, {4, 5}, {6}, {6}, {7}, {}, {1, 9}, {10}, {8}, {9}};
         System.out.println(eventualSafeNodes(graph2));
+
+        System.out.println(eventualSafeNodesBFS(graph2));
     }
 
 }
