50c370df545301116533cd990e9d8b672460727e
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-03-12 23:23:50 +0530

    DFS Technique added in AlienDictionary Problem

--- Graphs/AlienDictionary.java
+++ Graphs/AlienDictionary.java
@@ -1,25 +1,80 @@
 package Graphs;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class AlienDictionary {
 
     // Given a sorted dictionary of an alien language having N words and k starting alphabets of a standard dictionary.
     // Find the order of characters in the alien language.
 
+    // 1. DFS
+    static String alienOrder(String[] words) {
+        Map<Character, Set<Character>> adj = new HashMap<>();
+        Map<Character, Boolean> visit = new HashMap<>(); // False-visited, True=current path
+        StringBuilder res = new StringBuilder();
+
+        for (String w : words) {
+            for (char c : w.toCharArray()) {
+                adj.put(c, new HashSet<>());
+            }
+        }
+
+        for (int i = 0; i < words.length - 1; i++) {
+            String w1 = words[i];
+            String w2 = words[i + 1];
+            int minLen = Math.min(w1.length(), w2.length());
+
+            if (w1.length() > w2.length() && w1.substring(0, minLen).equals(w2.substring(0, minLen))) {
+                return "";
+            }
+
+            for (int j = 0; j < minLen; j++) {
+                if (w1.charAt(j) != w2.charAt(j)) {
+                    adj.get(w1.charAt(j)).add(w2.charAt(j));
+                    break;
+                }
+            }
+        }
+
+        for (Character c : adj.keySet()) {
+            if (dfs(adj, visit, res, c)) {
+                return "";
+            }
+        }
+
+        return res.reverse().toString();
+    }
+
+    private static boolean dfs(Map<Character, Set<Character>> adj, Map<Character, Boolean> visit, StringBuilder res, Character c) {
+        if (visit.containsKey(c)) {
+            return visit.get(c);
+        }
+
+        visit.put(c, true);
+        for (Character i : adj.get(c)) {
+            if (dfs(adj, visit, res, i)) {
+                return true;
+            }
+        }
+
+        visit.put(c, false);
+        res.append(c);
+        return false;
+    }
+
+
+    // 2. Topological Sort (Kahn's Algorithm)
     static String findOrder(String[] dict, int n, int k) {
         List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i < k; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             String s1 = dict[i];
             String s2 = dict[i + 1];
             int len = Math.min(s1.length(), s2.length());
+
             for (int j = 0; j < len; j++) {
                 if (s1.charAt(j) != s2.charAt(j)) {
                     graph.get(s1.charAt(j) - 'a').add(s2.charAt(j) - 'a');
@@ -29,12 +84,12 @@ public class AlienDictionary {
         }
 
         List<Integer> topoSort = topologicalSort(graph, k);
-        String ans = "";
+        StringBuilder ans = new StringBuilder();
         for (int i : topoSort) {
-            ans += (char) (i + (int) ('a'));
+            ans.append((char) (i + (int) ('a')));
         }
 
-        return ans;
+        return ans.toString();
     }
 
     private static List<Integer> topologicalSort(List<List<Integer>> graph, int k) {
@@ -70,9 +125,12 @@ public class AlienDictionary {
 
 
     public static void main(String[] args) {
+        String[] words = {"wrt", "wrf", "er", "ett", "rftt"};
+
         String[] dict = {"baa", "abcd", "abca", "cab", "cad"};
         int n = 5, k = 4;
 
+        System.out.println(alienOrder(words));
         System.out.println(findOrder(dict, n, k));
     }
 
