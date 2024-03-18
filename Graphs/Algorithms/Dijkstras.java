50c00b5d21eb2804dac51bf6215fc57f6d06ed88
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-03-18 10:40:59 +0530

    Dijkstras Algorithm Updated

--- Graphs/Algorithms/Dijkstras.java
+++ Graphs/Algorithms/Dijkstras.java
@@ -25,6 +25,22 @@ public class Dijkstras {
     // SC - O(V)
 
 
+
+
+
+    // using ArrayList<Edge>[]
+
+    static class Edge {
+        int src, dest, weight;
+
+        Edge(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+    }
+
+
     static class Pair implements Comparable<Pair> {
         int node, weight;
 
@@ -35,33 +51,37 @@ public class Dijkstras {
 
         @Override
         public int compareTo(Pair o) {
-            return this.weight - o.weight;  // Ascending order sorting
+            return this.weight - o.weight;
         }
     }
 
 
-    static int[] dijkstra(ArrayList<ArrayList<Pair>> graph, int vertices, int src) {
+    static int[] dijkstra(ArrayList<Edge>[] graph, int vertices, int source) {
         PriorityQueue<Pair> pq = new PriorityQueue<>();
         int[] dist = new int[vertices];
         Arrays.fill(dist, Integer.MAX_VALUE);
         boolean[] visited = new boolean[vertices];
-        dist[src] = 0;
-        pq.add(new Pair(src, 0));
+
+        dist[source] = 0;
+        pq.add(new Pair(source, 0));
 
         while (!pq.isEmpty()) {
             Pair curr = pq.remove();
             int vertex = curr.node;
-
             if (!visited[vertex]) {
                 visited[vertex] = true;
-                for (Pair p : graph.get(vertex)) {
-                    int node = p.node;
-                    int weight = p.weight;
-                    if (dist[vertex] + weight < dist[node]) {  // relaxation
-                        dist[node] = dist[vertex] + weight;
-                        pq.add(new Pair(node, dist[node]));
+
+                for (int i = 0; i < graph[vertex].size(); i++) {
+                    Edge e = graph[vertex].get(i);
+                    int src = e.src;
+                    int dest = e.dest;
+                    int weight = e.weight;
+                    if (dist[src] + weight < dist[dest]) {  // relaxation
+                        dist[dest] = dist[src] + weight;
+                        pq.add(new Pair(dest, dist[dest]));
                     }
                 }
+
             }
         }
 
@@ -71,52 +91,134 @@ public class Dijkstras {
 
     public static void main(String[] args) {
         int vertices = 6;
-        ArrayList<ArrayList<Pair>> graph = new ArrayList<>();
+        ArrayList<Edge>[] graph = new ArrayList[vertices];
         for (int i = 0; i < vertices; i++) {
-            graph.add(new ArrayList<>());
+            graph[i] = new ArrayList<>();
         }
 
-        // Undirected
-//        graph.get(0).add(new Pair(1, 4));
+        graph[0].add(new Edge(0, 1, 4));
+        graph[0].add(new Edge(0, 2, 4));
+
+        graph[1].add(new Edge(1, 0, 4));
+        graph[1].add(new Edge(1, 2, 2));
+
+        graph[2].add(new Edge(2, 0, 4));
+        graph[2].add(new Edge(2, 1, 2));
+        graph[2].add(new Edge(2, 3, 3));
+        graph[2].add(new Edge(2, 4, 1));
+        graph[2].add(new Edge(2, 5, 6));
+
+        graph[3].add(new Edge(3, 2, 3));
+        graph[3].add(new Edge(3, 5, 2));
+
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 5, 3));
+
+        graph[5].add(new Edge(5, 2, 6));
+        graph[5].add(new Edge(5, 3, 2));
+        graph[5].add(new Edge(5, 4, 3));
+
+        int src = 1;
+        System.out.println(Arrays.toString(dijkstra(graph, vertices, src)));
+    }
+
+
+
+
+    // using ArrayList<ArrayList<Pair>>
+
+//    static class Pair implements Comparable<Pair> {
+//        int node, weight;
+//
+//        Pair(int node, int weight) {
+//            this.node = node;
+//            this.weight = weight;
+//        }
+//
+//        @Override
+//        public int compareTo(Pair o) {
+//            return this.weight - o.weight;  // Ascending order sorting
+//        }
+//    }
+//
+//
+//    static int[] dijkstra(ArrayList<ArrayList<Pair>> graph, int vertices, int src) {
+//        PriorityQueue<Pair> pq = new PriorityQueue<>();
+//        int[] dist = new int[vertices];
+//        Arrays.fill(dist, Integer.MAX_VALUE);
+//        boolean[] visited = new boolean[vertices];
+//        dist[src] = 0;
+//        pq.add(new Pair(src, 0));
+//
+//        while (!pq.isEmpty()) {
+//            Pair curr = pq.remove();
+//            int vertex = curr.node;
+//
+//            if (!visited[vertex]) {
+//                visited[vertex] = true;
+//                for (Pair p : graph.get(vertex)) {
+//                    int node = p.node;
+//                    int weight = p.weight;
+//                    if (dist[vertex] + weight < dist[node]) {  // relaxation
+//                        dist[node] = dist[vertex] + weight;
+//                        pq.add(new Pair(node, dist[node]));
+//                    }
+//                }
+//            }
+//        }
+//
+//        return dist;
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        int vertices = 6;
+//        ArrayList<ArrayList<Pair>> graph = new ArrayList<>();
+//        for (int i = 0; i < vertices; i++) {
+//            graph.add(new ArrayList<>());
+//        }
+//
+//        // Undirected
+////        graph.get(0).add(new Pair(1, 4));
+////        graph.get(0).add(new Pair(2, 4));
+////
+////        graph.get(1).add(new Pair(0, 4));
+////        graph.get(1).add(new Pair(2, 2));
+////
+////        graph.get(2).add(new Pair(0, 4));
+////        graph.get(2).add(new Pair(1, 2));
+////        graph.get(2).add(new Pair(3, 3));
+////        graph.get(2).add(new Pair(4, 1));
+////        graph.get(2).add(new Pair(5, 6));
+////
+////        graph.get(3).add(new Pair(2, 3));
+////        graph.get(3).add(new Pair(5, 2));
+////
+////        graph.get(4).add(new Pair(2, 1));
+////        graph.get(4).add(new Pair(5, 3));
+////
+////        graph.get(5).add(new Pair(2, 6));
+////        graph.get(5).add(new Pair(3, 2));
+////        graph.get(5).add(new Pair(4, 3));
+//
+//        // Directed
+//        graph.get(0).add(new Pair(1, 2));
 //        graph.get(0).add(new Pair(2, 4));
 //
-//        graph.get(1).add(new Pair(0, 4));
-//        graph.get(1).add(new Pair(2, 2));
+//        graph.get(1).add(new Pair(2, 1));
+//        graph.get(1).add(new Pair(3, 7));
 //
-//        graph.get(2).add(new Pair(0, 4));
-//        graph.get(2).add(new Pair(1, 2));
-//        graph.get(2).add(new Pair(3, 3));
-//        graph.get(2).add(new Pair(4, 1));
-//        graph.get(2).add(new Pair(5, 6));
+//        graph.get(2).add(new Pair(4, 3));
 //
-//        graph.get(3).add(new Pair(2, 3));
-//        graph.get(3).add(new Pair(5, 2));
+//        graph.get(3).add(new Pair(5, 1));
 //
-//        graph.get(4).add(new Pair(2, 1));
-//        graph.get(4).add(new Pair(5, 3));
+//        graph.get(4).add(new Pair(3, 2));
+//        graph.get(4).add(new Pair(5, 5));
 //
-//        graph.get(5).add(new Pair(2, 6));
-//        graph.get(5).add(new Pair(3, 2));
-//        graph.get(5).add(new Pair(4, 3));
-
-        // Directed
-        graph.get(0).add(new Pair(1, 2));
-        graph.get(0).add(new Pair(2, 4));
-
-        graph.get(1).add(new Pair(2, 1));
-        graph.get(1).add(new Pair(3, 7));
-
-        graph.get(2).add(new Pair(4, 3));
-
-        graph.get(3).add(new Pair(5, 1));
-
-        graph.get(4).add(new Pair(3, 2));
-        graph.get(4).add(new Pair(5, 5));
-
-
-        int src = 0;
-        System.out.println(Arrays.toString(dijkstra(graph, vertices, src)));
-
-    }
+//
+//        int src = 0;
+//        System.out.println(Arrays.toString(dijkstra(graph, vertices, src)));
+//
+//    }
 
 }
