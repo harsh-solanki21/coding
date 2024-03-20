5dab537c018bde3af905965df2c1cb2b846831c9
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-03-20 11:38:58 +0530

    ProductExceptSelf Updated

--- Arrays/ProductExceptSelf.java
+++ Arrays/ProductExceptSelf.java
@@ -6,23 +6,9 @@ public class ProductExceptSelf {
 
     // https://leetcode.com/problems/product-of-array-except-self/description/
 
-    static public int[] productExceptSelf(int[] nums) {
-        int[] ans = new int[nums.length];
-        ans[0] = 1;
-        for (int i = 1; i < nums.length; i++) {
-            ans[i] = ans[i - 1] * nums[i - 1];
-        }
 
-        int prod = 1;
-        for (int j = nums.length - 2; j >= 0; j--) {
-            prod *= nums[j + 1];
-            ans[j] *= prod;
-        }
-
-        return ans;
-    }
-
-    static int[] productExceptSelf2(int[] nums) {
+    // TC - O(n), SC - O(3n)
+    static int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] pre = new int[n];
         int[] suff = new int[n];
@@ -44,7 +30,9 @@ public class ProductExceptSelf {
         return ans;
     }
 
-    static int[] productExceptSelf3(int[] nums) {
+
+    // TC - O(n), SC - O(~2n)
+    static int[] productExceptSelf2(int[] nums) {
         int[] ans = new int[nums.length];
         Arrays.fill(ans, 1);
         int curr = 1;
@@ -63,9 +51,28 @@ public class ProductExceptSelf {
         return ans;
     }
 
+
+    // TC - O(n), SC - O(n)
+    static public int[] productExceptSelf3(int[] nums) {
+        int[] ans = new int[nums.length];
+        ans[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+
+        int prod = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            prod *= nums[i + 1];
+            ans[i] *= prod;
+        }
+
+        return ans;
+    }
+
+
     public static void main(String[] args) {
-//        int[] nums = {1, 2, 3, 4};
-        int[] nums = {-1, 1, 0, -3, 3};
+        int[] nums = {2, 3, 4, 5, 2, 3};
+//        int[] nums = {-1, 1, 0, -3, 3};
         System.out.println(Arrays.toString(productExceptSelf(nums)));
         System.out.println(Arrays.toString(productExceptSelf2(nums)));
         System.out.println(Arrays.toString(productExceptSelf3(nums)));
