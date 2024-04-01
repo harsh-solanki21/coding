61c3c40dc8728aace6cb8d2e1cfe8158c81996b5
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-04-01 17:17:18 +0530

    Main Updated

--- Main.java
+++ Main.java
@@ -2,47 +2,8 @@ import java.util.*;
 
 public class Main {
 
+    // Tarjan’s Algorithm to find Strongly Connected Components
 
 
-
-
-
-//    public static void main(String[] args) {
-//
-////        0 -- 1 --- 4
-////        |    |    |  \
-////        2 -- 3    5 - 6
-//
-//        int vertices = 7;
-//        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-//
-//        for (int i = 0; i < vertices; i++) {
-//            graph.add(new ArrayList<>());
-//        }
-//
-//        graph.get(0).add(1);
-//        graph.get(0).add(2);
-//
-//        graph.get(1).add(0);
-//        graph.get(1).add(3);
-//        graph.get(1).add(4);
-//
-//        graph.get(2).add(0);
-//        graph.get(2).add(3);
-//
-//        graph.get(3).add(1);
-//        graph.get(3).add(2);
-//
-//        graph.get(4).add(1);
-//        graph.get(4).add(5);
-//        graph.get(4).add(6);
-//
-//        graph.get(5).add(4);
-//        graph.get(5).add(6);
-//
-//        graph.get(6).add(4);
-//        graph.get(6).add(5);
-//
-//    }
 
 }
