a152d85ee0360dee1a6fdaf2a6df89a86a8cc5cb
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-04-23 15:31:03 +0530

    Prototype Pattern

---
+++ LowLevelDesign/DesignPatterns/CreationalPatterns/Prototype/Employee.java
@@ -0,0 +1,40 @@
+package LowLevelDesign.DesignPatterns.CreationalPatterns.Prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee implements Cloneable {
+
+    private List<String> empList;
+
+    public Employee() {
+        empList = new ArrayList<>();
+    }
+
+    public Employee(List<String> list) {
+        this.empList = list;
+    }
+
+    public void loadData() {
+        // read all employees from database and put into the list
+        empList.add("Pankaj");
+        empList.add("Raj");
+        empList.add("David");
+        empList.add("Lisa");
+    }
+
+    public List<String> getEmpList() {
+        return empList;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        // clone method is overridden to provide a deep copy of the employee list
+        List<String> temp = new ArrayList<>();
+        for (String s : this.getEmpList()) {
+            temp.add(s);
+        }
+        return new Employee(temp);
+    }
+
+}
---
+++ LowLevelDesign/DesignPatterns/CreationalPatterns/Prototype/Prototype.md
@@ -0,0 +1,14 @@
+# Prototype Pattern 
+
+- Prototype pattern is used when Object creation is a costly affair and requires a lot of time and resources, and you have a similar object already existing.
+
+- So this pattern provides a mechanism to copy the original object to a new object and then modify it according to our needs.
+
+
+- The concept is to copy an existing object rather than creating a new instance from scratch, because creating a new object may be costly.
+
+- This approach saves costly resources and time, especially when object is a heavy process.
+
+<br /> 
+
+> Whether to use shallow or deep copy of the Object properties depends on the requirements, and it’s a design decision.
