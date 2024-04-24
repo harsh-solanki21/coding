26b180ad40513e220cab93c255c9df72c7e7fd00
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-04-24 15:24:43 +0530

    Flyweight Pattern

---
+++ LowLevelDesign/DesignPatterns/StructuralPatterns/Flyweight/Flyweight.md
@@ -0,0 +1,27 @@
+# Flyweight Pattern
+
+>According to GoF, Flyweight design pattern intent is:
+<br />
+> **Use sharing to support large numbers of fine-grained objects efficiently.**
+
+<br />
+
+- Flyweight design pattern is used when we need to create a lot of Objects of a class. Since every object consumes memory space that can be crucial for low memory devices, such as mobile devices or embedded systems, flyweight design pattern can be applied to reduce the load on memory by sharing objects.
+
+<br />
+
+- Before we apply flyweight design pattern, we need to consider following
+factors:
+  1. The number of Objects to be created by application should be huge.
+  2. The object creation is heavy on memory, and it can be time-consuming
+too.
+  3. The object properties can be divided into intrinsic and extrinsic properties, extrinsic properties of an Object should be defined by the
+  client program.
+
+<br />
+
+- To apply flyweight pattern, we need to divide Object property into intrinsic and extrinsic properties. Intrinsic properties make the Object unique whereas extrinsic properties are set by client code and used to perform different operations. For example, an Object Circle can have extrinsic properties such as color and width.
+
+
+- For applying flyweight pattern, we need to create a Flyweight factory that
+  returns the shared objects. For our example, let’s say we need to create a drawing with lines and Ovals. So we will have an interface Shape and its concrete implementations as Line and Oval. Oval class will have intrinsic property to determine whether to fill the Oval with given color or not whereas Line will not have any intrinsic property.
--- Main.java
+++ Main.java
@@ -3,53 +3,51 @@ import java.util.*;
 public class Main {
 
 
-
-}
-
 //    static List<Integer> eventualSafeNode(ArrayList<ArrayList<Integer>> graph, int vertices) {
 //
 //    }
-//
-//
-//    public static void main(String[] args) {
-//        // 1. ArrayList<ArrayList<Integer>>
-//
-////            0 -> 1 -> 2 -> 3 -> 5
-////                 ↑         ↓    ↓
-////                 8         4 -> 6 -> 7
-////               ↓   ↑
-////        11 -> 9 -> 10
-//
-//        int vertices = 12;
-//        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-//
-//        for (int i = 0; i < vertices; i++) {
-//            graph.add(new ArrayList<>());
-//        }
-//
-//        graph.get(0).add(1);
-//
-//        graph.get(1).add(2);
-//
-//        graph.get(2).add(3);
-//
-//        graph.get(3).add(4);
-//        graph.get(3).add(5);
-//
-//        graph.get(4).add(6);
-//
-//        graph.get(5).add(6);
-//
-//        graph.get(6).add(7);
-//
-//        graph.get(8).add(1);
-//        graph.get(8).add(9);
-//
-//        graph.get(9).add(10);
-//
-//        graph.get(10).add(8);
-//
-//        graph.get(11).add(9);
-//
+
+
+    public static void main(String[] args) {
+
+//            0 -> 1 -> 2 -> 3 -> 5
+//                 ↑         ↓    ↓
+//                 8         4 -> 6 -> 7
+//               ↓   ↑
+//        11 -> 9 -> 10
+
+        int vertices = 12;
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < vertices; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        graph.get(0).add(1);
+
+        graph.get(1).add(2);
+
+        graph.get(2).add(3);
+
+        graph.get(3).add(4);
+        graph.get(3).add(5);
+
+        graph.get(4).add(6);
+
+        graph.get(5).add(6);
+
+        graph.get(6).add(7);
+
+        graph.get(8).add(1);
+        graph.get(8).add(9);
+
+        graph.get(9).add(10);
+
+        graph.get(10).add(8);
+
+        graph.get(11).add(9);
+
 //        System.out.println(eventualSafeNode(graph, vertices));
-//    }
+    }
+
+}
