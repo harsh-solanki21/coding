eb4694edb536987b36413220e5de4d385cc4a2eb
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-04-24 18:22:48 +0530

    Main Update

--- Main.java
+++ Main.java
@@ -3,51 +3,18 @@ import java.util.*;
 public class Main {
 
 
-//    static List<Integer> eventualSafeNode(ArrayList<ArrayList<Integer>> graph, int vertices) {
-//
-//    }
+    static List<Integer> eventualSafeNodes(int[][] graph) {
+
+    }
+
+    private static boolean dfs(int[][] graph) {
+
+    }
 
 
     public static void main(String[] args) {
-
-//            0 -> 1 -> 2 -> 3 -> 5
-//                 ↑         ↓    ↓
-//                 8         4 -> 6 -> 7
-//               ↓   ↑
-//        11 -> 9 -> 10
-
-        int vertices = 12;
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-
-        for (int i = 0; i < vertices; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        graph.get(0).add(1);
-
-        graph.get(1).add(2);
-
-        graph.get(2).add(3);
-
-        graph.get(3).add(4);
-        graph.get(3).add(5);
-
-        graph.get(4).add(6);
-
-        graph.get(5).add(6);
-
-        graph.get(6).add(7);
-
-        graph.get(8).add(1);
-        graph.get(8).add(9);
-
-        graph.get(9).add(10);
-
-        graph.get(10).add(8);
-
-        graph.get(11).add(9);
-
-//        System.out.println(eventualSafeNode(graph, vertices));
+        int[][] graph = {{1}, {2}, {3}, {4, 5}, {6}, {6}, {7}, {}, {1, 9}, {10}, {8}, {9}};
+        System.out.println(eventualSafeNodes(graph));
     }
 
 }
