a9c0fb252e670fba2482b794c6e4a9f8f71a13f6
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-06-04 11:53:33 +0530

    DFS Traversal

--- Graphs/Algorithms/DFS/DFSListPreferred.java
+++ Graphs/Algorithms/DFS/DFSListPreferred.java
@@ -5,69 +5,65 @@ import java.util.ArrayList;
 public class DFSListPreferred {
 
     static class Edge {
-        int source, destination;
+        int src, dest;
 
-        Edge(int source, int destination) {
-            this.source = source;
-            this.destination = destination;
+        Edge(int src, int dest) {
+            this.src = src;
+            this.dest = dest;
         }
     }
 
 
     // TC - O(V + E)
-    static void dfs(ArrayList<Edge>[] graph, boolean[] visited, int current) {
-        System.out.print(current + " ");
-        visited[current] = true;
+    static ArrayList<Integer> dfs(ArrayList<Edge>[] graph, int vertices) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        boolean[] visited = new boolean[vertices];
+        for (int i = 0; i < vertices; i++) {
+            if (!visited[i]) {
+                dfsTraversal(graph, visited, ans, i);
+            }
+        }
+        return ans;
+    }
 
-        for (int i = 0; i < graph[current].size(); i++) {
-            Edge e = graph[current].get(i);
-            if (!visited[e.destination]) {
-                dfs(graph, visited, e.destination);
+    private static void dfsTraversal(ArrayList<Edge>[] graph, boolean[] visited, ArrayList<Integer> ans, int node) {
+        visited[node] = true;
+        ans.add(node);
+
+        for (Edge e : graph[node]) {
+            if (!visited[e.dest]) {
+                dfsTraversal(graph, visited, ans, e.dest);
             }
         }
     }
 
 
-    static void createGraph(ArrayList<Edge>[] graph) {
-        for (int i = 0; i < graph.length; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        graph[0].add(new Edge(0, 1));
-        graph[0].add(new Edge(0, 3));
-
-        graph[1].add(new Edge(1, 0));
-        graph[1].add(new Edge(1, 2));
-        graph[1].add(new Edge(1, 3));
-
-        graph[2].add(new Edge(2, 1));
-        graph[2].add(new Edge(2, 3));
-
-        graph[3].add(new Edge(3, 0));
-        graph[3].add(new Edge(3, 1));
-        graph[3].add(new Edge(3, 2));
-
-        graph[4].add(new Edge(4, 5));
-
-        graph[5].add(new Edge(5, 4));
+    private static void addEdge(ArrayList<Edge>[] graph, int u, int v) {
+        graph[u].add(new Edge(u, v));
+        graph[v].add(new Edge(v, u));
     }
 
 
     public static void main(String[] args) {
-        int vertices = 7;
-        ArrayList<Edge>[] graph = new ArrayList[vertices];
-        createGraph(graph);
         /*
               0 --- 1
               |  /  |     4 --- 5    6
               3 --- 2
          */
-        boolean[] visited = new boolean[vertices];
-        for (int i = 0; i < vertices; i++) {
-            if (!visited[i]) {
-                dfs(graph, visited, i);
-            }
+        int vertices = 7;
+        ArrayList<Edge>[] graph = new ArrayList[vertices];
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
         }
+
+        addEdge(graph, 0, 1);
+        addEdge(graph, 0, 3);
+        addEdge(graph, 1, 2);
+        addEdge(graph, 1, 3);
+        addEdge(graph, 2, 3);
+        addEdge(graph, 4, 5);
+
+        System.out.println(dfs(graph, vertices));
     }
 
 }
